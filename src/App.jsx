b6c80ef909e2056b09c9fb5465ddcc4262c007be
Gabriel Acosta <marcelo.acosta1@claro.com.ar>
2023-08-06 22:48:06 -0300

    mejoras de imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import NavBarr from "./component/NavBar";
 import Welcome from "./component/Welcome";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "/App.css";
-import Contador from "./component/Contador";
 import ItemListContainer from "./component/ItemListContainer";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Evento from "./component/Evento";
 import ItemDetailContainer from "./component/ItemDetailContainer";
 
 function App() {
@@ -20,9 +18,6 @@ function App() {
         <Route exact  path="/products/:category" element={ <ItemListContainer /> }/>
         <Route exact  path="/item/:id" element={ <ItemDetailContainer /> }/>
 
-
-        {/* <Contador
-      stock={7}/> */}
       </Routes>
     </BrowserRouter>
  
